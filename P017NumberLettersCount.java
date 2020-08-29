8327c2cc237355e9d0dca980c9057c6f0d6385c6
Ricardo <ricardo.serrato@expertfect.com>
2020-08-29 13:24:51 -0500

    Problem 18

--- P017NumberLettersCount.java
+++ P017NumberLettersCount.java
@@ -32,7 +32,7 @@ public class P017NumberLettersCount implements IProblem {
     @Override
     public void solve() {
         long count = 0;
-        for (int i = 342 ; i <= 342  ; i++) {
+        for (int i = 1; i <= 1000; i++) {
             System.out.println(toString(i));
             count += toString(i).replace(" ", "").length();
         }
@@ -40,30 +40,34 @@ public class P017NumberLettersCount implements IProblem {
     }
 
     private String toString(int number) {
-        String numberString = "";
-        int thounsands = number / 1000;
 
+        String numberString = "";
+
+        int thounsands = number / 1000;
         if (thounsands > 0){
             numberString += numberString.isEmpty() ? "" : " and ";
             numberString = Numbers[thounsands - 1] + " thousand";
         }
 
-        int hundreds = (number - (thounsands * 1000)) / 100;
+        number -= thounsands * 1000;
+        int hundreds = number / 100;
         if (hundreds > 0) {
             numberString += numberString.isEmpty() ? "" : " and ";
             numberString += Numbers[hundreds - 1] + " hundred";
         }
 
-        int tens = (number - (thounsands * 1000) - (hundreds * 100)) / 10;
+        number -= hundreds * 100;
+        int tens = number / 10;
         if (tens >= 2) {
             numberString += numberString.isEmpty() ? "" : " and ";
             numberString += TensNumbers[tens - 1];
         }
 
-        int units = (number - (thounsands * 1000) - (hundreds * 100)) - 20;
+        number -= (tens >= 2 ? tens * 10 : 0);
+        int units = number;
         if (units > 0 && units<= 20) {
             numberString += numberString.isEmpty() ? "" : 
-                            (thounsands> 0 || hundreds>0) && tens <=0 ? " and " : "";
+                            tens < 2 && units<20 ? " and " : "";
             numberString += Numbers[units - 1];
         }
        
