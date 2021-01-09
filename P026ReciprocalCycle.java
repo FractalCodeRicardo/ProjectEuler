7f15a385b57b959ed47866ca31ecded251c42d70
Ricardo <ricardo.serrato@expertfect.com>
2021-01-09 09:50:43 -0600

    Problem 26

--- P026ReciprocalCycle.java
+++ P026ReciprocalCycle.java
@@ -1,37 +1,68 @@
+import java.time.format.DecimalStyle;
+
 public class P026ReciprocalCycle implements IProblem {
+
+    private int d = 1000;
 
     @Override
     public void solve() {
         
-        int longestCycle = 0;
-        int d = 0;
-        for (int i = 7; i <= 7; i++) {
-            double number = 1.0/(double)i;
-        
-            int tLongestCycle = getRecurringCycle(i, number);
+        int longest = 0;
+        int longestD = 0;
+        for (int i = 2; i <= d; i++) {
+            double number = 1.0 / i;
+            int ocurrences = getMaxOcurrences(number);
 
-            if (tLongestCycle > 0 && longestCycle < tLongestCycle) {
-                longestCycle = tLongestCycle;
-                d = i;
+            if (ocurrences > longest) {
+                longest = ocurrences;
+                longestD = i;
             }
-            
-        }
 
-        System.out.println(d);
+            System.out.println(i + " " + number + " " + ocurrences);
+        }
+        
+        System.out.println(longestD);
+
     }
 
-    private int isCycle(double number) {
+    private int getMaxOcurrences(double number) {
         String decimals = (number + "").split("\\.")[1];
-    
-        for (int i = 1; i < decimals.length() / 2; i--) {
-            
-
-            
-            
-            
-        }
         
+        int size = 1;
+        int t = (int)Math.floor(decimals.length() / 2);
+        while (size <= t) {
+
+            for (int i = 0; i < decimals.length() - size; i++) {
+
+                String first = decimals.substring(i, i + size);
+                String second = decimals.substring(i + size , Math.min(i + size + size, decimals.length()));
+
+                if (first.equals(second)) {
+                    return count(first);
+                }
+            }
+
+            size++;
+        }
+
         return 0;
+    }
+
+
+    private int count(String l) {
+        char current = l.charAt(0);
+        int c = 1;
+        
+        for (int i = 1; i < l.length(); i++) {
+            if (current != l.charAt(i)) {
+                c++;
+                current = l.charAt(i);
+                continue;
+            }
+
+            break;
+        }
+        return c;
     }
 
     
